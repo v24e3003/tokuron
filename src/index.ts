0c23f36f2a57c19e86540339c524111d8bb9e005
v24e3003 <v24e3003@oita-u.ac.jp>
2025-06-01 20:16:46 +0900

    honoでOpenAPIの確認

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Hono } from 'hono'
-
+import { logger } from 'hono/logger'
+import { zValider } from '@hono/zod-valodator'
+import { z, createRoute, OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
 const app = new Hono()
 
 app.get('/', (c) => {
@@ -10,5 +13,6 @@ app.get('/api', (c) => {
   const query = c.req.query('name');
   return c.json({message: `Hello ${query}!`})
 })
+
 
 export default app
