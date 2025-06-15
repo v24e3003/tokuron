2a7e296448d61d3aea6236b3f42b92b19b7f2bd5
Eto Takayuki <v24e3003@oita-u.ac.jp>
2025-06-15 10:10:12 +0000

    不要なコードの削除

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,9 @@
 import { Hono } from 'hono'
-import { logger } from 'hono/logger'
 import { zValidator } from '@hono/zod-validator'
 import { z, createRoute, OpenAPIHono } from '@hono/zod-openapi'
 import { swaggerUI } from '@hono/swagger-ui'
-//const app = new Hono()
+
 const app = new OpenAPIHono()
-
-app.use(logger())
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-// app.get('/api', (c) => {
-//   const query = c.req.query('name');
-//   return c.json({message: `Hello ${query}!`})
-// })
-
-app.openapi(
-  createRoute({
-    method: 'get',
-    path: '/api',
-    request: {
-      params: z.object({name: z.string().optional()})
-    },
-    responses: {
-      200: {
-        description: 'Respond a hello message',
-        content: {
-          'application/json': {schema: z.object({message: z.string()})}
-        }
-      }
-    }
-  }),
-  (c) => {
-    const query = c.req.query('name');
-    return c.json({message: `Hello ${query}`})
-  }
-)
 
 app.doc('/doc', {
   info: {
@@ -53,13 +19,6 @@ app.get('/ui',
   })
 )
 
-
-app.post('/api',
-  zValidator('form', z.object({body: z.string()})),
-  (c) => {
-    const valudated = c.req.valid('form')
-    return c.text('POST /api')
-  })
 app.notFound((c) => c.text('Custom 404 Message', 404))
 
 export default app
